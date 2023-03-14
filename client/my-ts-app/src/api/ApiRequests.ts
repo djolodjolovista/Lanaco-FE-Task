57e2eeb89b0a7fd3bc4b5fe65680c783abb6b96d
djolodjolo <tkd_djole@rocketmail.com>
2023-03-14 16:23:07 +0100

    Added env

--- client/my-ts-app/src/api/ApiRequests.ts
+++ client/my-ts-app/src/api/ApiRequests.ts
@@ -3,105 +3,105 @@ import axios from 'axios';
 export const api = {
   getAllCustomers: () => {
     try {
-      return axios.get('https://63fd283e8ef914c555a635e6.mockapi.io/lanaco/customers');
+      return axios.get(`${process.env.REACT_APP_BASE_URL_2}/customers`);
     } catch (error) {
       console.log(error);
     }
   },
   getAllSellers: () => {
     try {
-      return axios.get('https://63fd1fd28ef914c555a5c035.mockapi.io/lanaco/sellers');
+      return axios.get(`${process.env.REACT_APP_BASE_URL_1}/sellers`);
     } catch (error) {
       console.log(error);
     }
   },
   getAllInvoices: () => {
     try {
-      return axios.get('https://63fd1fd28ef914c555a5c035.mockapi.io/lanaco/invoices');
+      return axios.get(`${process.env.REACT_APP_BASE_URL_1}/invoices`);
     } catch (error) {
       console.log(error);
     }
   },
   deleteSeller: (id: string) => {
     try {
-      return axios.delete(`https://63fd1fd28ef914c555a5c035.mockapi.io/lanaco/sellers/${id}`);
+      return axios.delete(`${process.env.REACT_APP_BASE_URL_1}/sellers/${id}`);
     } catch (error) {
       console.log(error);
     }
   },
   createSeller: (body: any) => {
     try {
-      return axios.post(`https://63fd1fd28ef914c555a5c035.mockapi.io/lanaco/sellers`, body);
+      return axios.post(`${process.env.REACT_APP_BASE_URL_1}/sellers`, body);
     } catch (error) {
       console.log(error);
     }
   },
   updateSeller: (id: string, body: any) => {
     try {
-      return axios.put(`https://63fd1fd28ef914c555a5c035.mockapi.io/lanaco/sellers/${id}`, body);
+      return axios.put(`${process.env.REACT_APP_BASE_URL_1}/sellers/${id}`, body);
     } catch (error) {
       console.log(error);
     }
   },
   getSeller: (id: string) => {
     try {
-      return axios.get(`https://63fd1fd28ef914c555a5c035.mockapi.io/lanaco/sellers/${id}`);
+      return axios.get(`${process.env.REACT_APP_BASE_URL_1}/sellers/${id}`);
     } catch (error) {
       console.log(error);
     }
   },
   deleteCustomer: (id: string) => {
     try {
-      return axios.delete(`https://63fd283e8ef914c555a635e6.mockapi.io/lanaco/customers/${id}`);
+      return axios.delete(`${process.env.REACT_APP_BASE_URL_2}/customers/${id}`);
     } catch (error) {
       console.log(error);
     }
   },
   createCustomer: (body: any) => {
     try {
-      return axios.post(`https://63fd283e8ef914c555a635e6.mockapi.io/lanaco/customers`, body);
+      return axios.post(`${process.env.REACT_APP_BASE_URL_2}/customers`, body);
     } catch (error) {
       console.log(error);
     }
   },
   updateCustomer: (id: string, body: any) => {
     try {
-      return axios.put(`https://63fd283e8ef914c555a635e6.mockapi.io/lanaco/customers/${id}`, body);
+      return axios.put(`${process.env.REACT_APP_BASE_URL_2}/customers/${id}`, body);
     } catch (error) {
       console.log(error);
     }
   },
   getCustomer: (id: string) => {
     try {
-      return axios.get(`https://63fd283e8ef914c555a635e6.mockapi.io/lanaco/customers/${id}`);
+      return axios.get(`${process.env.REACT_APP_BASE_URL_2}/customers/${id}`);
     } catch (error) {
       console.log(error);
     }
   },
   getInvoice: (id: string) => {
     try {
-      return axios.get(`https://63fd1fd28ef914c555a5c035.mockapi.io/lanaco/invoices/${id}`);
+      return axios.get(`${process.env.REACT_APP_BASE_URL_1}/invoices/${id}`);
     } catch (error) {
       console.log(error);
     }
   },
   createInvoice: (body: any) => {
     try {
-      return axios.post('https://63fd1fd28ef914c555a5c035.mockapi.io/lanaco/invoices', body);
+      return axios.post(`${process.env.REACT_APP_BASE_URL_1}/invoices`, body);
     } catch (error) {
       console.log(error);
     }
   },
   updateInvoice: (id: string, body: any) => {
     try {
-      return axios.put(`https://63fd1fd28ef914c555a5c035.mockapi.io/lanaco/invoices/${id}`, body);
+      return axios.put(`${process.env.REACT_APP_BASE_URL_1}/invoices/${id}`, body);
     } catch (error) {
       console.log(error);
     }
   },
   deleteInvoice: (id: string) => {
     try {
-      return axios.delete(`https://63fd1fd28ef914c555a5c035.mockapi.io/lanaco/invoices/${id}`);
+      return axios.delete(`${process.env.REACT_APP_BASE_URL_1}/invoices/${id}`);
     } catch (error) {
       console.log(error);
     }
